acc0339c22e5d63ced0417dd6c18147630d34f0c
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-07-19 14:15:03 +0530

    Time: 0 ms (100%), Space: 9 MB (95.14%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        vector<int> last(26);
+
+        for(int i=0; i<n; i++){
+            last[s[i]-'a'] = i;
+        }
+
+        vector<bool> inSt(26, false);
+        string st;
+        
+        for(int i=0; i<n; i++){
+            char ch = s[i];
+            if(inSt[ch - 'a']) continue;
+
+            while(!st.empty() && st.back()>ch && last[st.back()-'a'] > i){
+                inSt[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+
+            st.push_back(ch);
+            inSt[ch - 'a'] = true;
+        }
+        return st;
+    }
+};

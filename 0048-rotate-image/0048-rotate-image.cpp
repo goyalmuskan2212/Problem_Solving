80c5f89e9dbd0655d1273cb640d7deed4b25b71c
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-07-22 15:52:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (93.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,12 +3,15 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1));
+        
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                ans[i][j] = matrix[m-j-1][i];
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        matrix = ans;
+
+        for(int i=0; i<n; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };

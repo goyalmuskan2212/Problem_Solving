5048d5ab0d72d4e5e5ecf1cf23e4f4fa960451d5
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-08-18 17:23:40 +0530

    Time: 6 ms (44.83%), Space: 23.7 MB (82.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> ans;
         for(int i=0; i<n; i++){
             if(ans.empty() || ans.back()[1] < intervals[i][0]){
-                ans.push_back({intervals[i]});
+                ans.push_back(intervals[i]);
             }
             else{
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);

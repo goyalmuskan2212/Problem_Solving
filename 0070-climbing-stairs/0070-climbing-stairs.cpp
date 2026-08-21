c41da7646a396d805b221e49fabf4a1cbd192b13
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-08-21 11:48:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,7 +7,14 @@ public:
         return dp[n] = ans;
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        vector<int> dp(n+1, 0);
+        int prev1 = 1;
+        int prev2 = 1;
+        for(int i=2; i<=n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };

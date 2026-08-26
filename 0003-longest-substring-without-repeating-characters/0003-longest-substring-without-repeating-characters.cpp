e1d3192cbc1525e46187e52a3e58a219484a1f01
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-08-26 23:14:53 +0530

    Time: 97 ms (21.79%), Space: 19.6 MB (20.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,14 @@ public:
         int n = s.size();
         int i=0;
         int j=0;
-        map<char, int> mpp;
         int maxi = 0;
+        map<int, int> mpp;
         while(j < n){
-            while(mpp.find(s[j]) != mpp.end()){
-                if(mpp[s[i]] == 1){
-                    mpp.erase(s[i]);
-                }
-                else mpp[s[i]]--;
+            mpp[s[j]]++;
+            while(mpp[s[j]] > 1){
+                mpp[s[i]]--;
                 i++;
             }
-            mpp[s[j]]++;
             maxi = max(maxi, j-i+1);
             j++;
         }

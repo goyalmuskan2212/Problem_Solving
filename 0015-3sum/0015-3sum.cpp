d4c20c9d91622cbd5659a08f5f8de14af2ab8df2
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-08-27 20:08:47 +0530

    Time: 53 ms (31.04%), Space: 31.2 MB (16.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,12 +5,10 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         for(int i=0; i<n; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
             int k = n-1;
-            while(j<k){
+            while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum < 0) j++;
                 else if(sum > 0) k--;

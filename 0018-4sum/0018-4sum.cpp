cfa905bd5fe026bb62eaa7129be5ae0b2a9162a3
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-08-27 20:16:46 +0530

    Time: 23 ms (54.01%), Space: 17.4 MB (56.8%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,28 +5,28 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         for(int i=0; i<n; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1; j<n; j++){
-                if(j != i+1 && nums[j] == nums[j-1]) continue;
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
                 int k = j+1;
                 int l = n-1;
-                while(k<l){
+                while(k < l){
                     long long sum = nums[i];
                     sum += nums[j];
                     sum += nums[k];
                     sum += nums[l];
-                    if(sum < target) k++;
-                    else if(sum > target) l--;
+                    if(sum < (long long)target) k++;
+                    else if(sum > (long long)target) l--;
                     else{
                         vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
                         sort(temp.begin(), temp.end());
                         ans.push_back(temp);
                         k++;
                         l--;
-                        while(k<l && nums[k] == nums[k-1]){
+                        while(k < l && nums[k] == nums[k-1]){
                             k++;
                         }
-                        while(k<l && nums[l] == nums[l+1]){
+                        while(k < l && nums[l] == nums[l+1]){
                             l--;
                         }
                     }

a453563249ff2e71df0e9cff75dabb67f1b44130
Muskan Goyal <146016535+goyalmuskan2212@users.noreply.github.com>
2026-09-04 19:51:57 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,7 +9,21 @@ public:
         return dp[m][n] = up + left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return solve(m-1, n-1, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+                dp[i][j] = (up + left);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
